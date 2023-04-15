3184a6cfd72fa7cf0c9a117931e7f7676e0e687e
João Borks <joao.borks@gmail.com>
2023-04-15 01:05:31 -0300

    refactor: simplify view sync

---
+++ Assets/_Project/Scripts/Runtime/Gameplay/Presentation/PresentationCharacterViewSystem.cs
@@ -0,0 +1,33 @@
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Transforms;
+
+namespace MyFps.Gameplay.Presentation
+{
+    [UpdateInGroup(typeof(PresentationSystemGroup))]
+    public partial class PresentationCharacterViewSystem : SystemBase
+    {
+        PresentationCharacterSystem _presentationSystem;
+
+        protected override void OnCreate()
+        {
+            _presentationSystem = World.GetExistingSystemManaged<PresentationCharacterSystem>();
+
+            var query = new EntityQueryBuilder(Allocator.Temp)
+                .WithAll<FirstPersonCharacterView>()
+                .WithAll<LocalToWorld>()
+                .Build(this);
+
+            RequireForUpdate(query);
+        }
+
+        protected override void OnUpdate()
+        {
+            foreach (var (view, transform) in SystemAPI.Query<RefRO<FirstPersonCharacterView>, RefRO<LocalToWorld>>())
+            {
+                if (_presentationSystem.TryGetBehaviorForEntity(EntityManager, view.ValueRO.CharacterEntity, out var behavior))
+                    behavior.View.SetPositionAndRotation(transform.ValueRO.Position, transform.ValueRO.Rotation);
+            }
+        }
+    }
+}
